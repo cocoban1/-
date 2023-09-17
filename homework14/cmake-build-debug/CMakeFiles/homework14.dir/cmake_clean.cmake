4b10a8e076504ca56fc2745b43ffc715bfcf41de
j8agent <2035966463@qq.com>
2023-09-17 12:05:47 +0800

    .

---
+++ homework14/cmake-build-debug/CMakeFiles/homework14.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework14.dir/main.c.obj"
+  "CMakeFiles/homework14.dir/main.c.obj.d"
+  "homework14.exe"
+  "homework14.exe.manifest"
+  "homework14.pdb"
+  "libhomework14.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/homework14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
